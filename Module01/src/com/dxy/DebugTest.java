af2d8c780230d88b81c8d1aa2bf78f327810420c
20177411Dxy <1439180389@qq.com>
2021-03-13 08:38:53 +0800

    Idea整合GitHub测试

---
+++ Module01/src/com/dxy/DebugTest.java
@@ -0,0 +1,34 @@
+package com.dxy;
+
+import java.util.HashMap;
+
+public class DebugTest {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        HashMap<String,String> map = new HashMap<>();
+        map.put("name","dxy");
+        map.put("age","21");
+        map.put("school","ShangDong");
+        map.put("major","computer");
+
+        String age = map.get("age");
+        System.out.println("age = " + age);
+
+        map.remove("major");
+        System.out.println(map);
+
+        Show();
+    }
+
+    public static void Show(){
+        try {
+            System.out.println("show something");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ out/artifacts/Module01_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 14391
+  Date: 2021/3/3
+  Time: 10:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>百度</title>
+  </head>
+  <body>
+  <h1 style="color: red">hello DXY</h1>
+  </body>
+</html>
